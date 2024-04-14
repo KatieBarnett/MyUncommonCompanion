d6ec62294fc4f9264424fb1d2dac0738174c7831
Katie Barnett <katiebarnett5@gmail.com>
2024-04-14 21:49:34 +1000

    Demo - User input validation

--- app/src/main/java/dev/katiebarnett/myuncommoncompanion/MainActivity.kt
+++ app/src/main/java/dev/katiebarnett/myuncommoncompanion/MainActivity.kt
@@ -42,9 +42,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Content(name: String, modifier: Modifier = Modifier) {
+fun Content(
+    name: String,
+    firstTextChangeInitialValue: Boolean = false,
+    modifier: Modifier = Modifier
+) {
     var input by remember { mutableStateOf<String>("") }
     var result by remember { mutableStateOf<String>("") }
+    var firstTextChange by remember { mutableStateOf<Boolean>(firstTextChangeInitialValue) }
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier.padding(16.dp)
@@ -58,14 +63,27 @@ fun Content(name: String, modifier: Modifier = Modifier) {
             value = input,
             onValueChange = {
                 input = it
+                firstTextChange = true
+            },
+            isError = firstTextChange && input.isNullOrBlank(),
+            supportingText = {
+                if (input.isNullOrBlank() && firstTextChange) {
+                    Text(
+                        text = "Please enter some text",
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
             },
             label = {
                 Text(text = "What type of pet would you like?")
             }
         )
-        Button(onClick = {
-            result = input
-        }) {
+        Button(
+            onClick = {
+                result = input
+            },
+            enabled = firstTextChange && !input.isNullOrBlank()
+        ) {
             Text(text = "Submit")
         }
         Text(text = result)
@@ -77,5 +95,13 @@ fun Content(name: String, modifier: Modifier = Modifier) {
 fun ContentPreview() {
     MyUncommonCompanionTheme {
         Content(stringResource(id = R.string.app_name))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContentPreviewInError() {
+    MyUncommonCompanionTheme {
+        Content(stringResource(id = R.string.app_name), firstTextChangeInitialValue = true)
     }
 }
