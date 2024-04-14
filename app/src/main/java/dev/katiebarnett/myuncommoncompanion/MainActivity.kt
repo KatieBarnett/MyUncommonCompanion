bdf1434ebd1f9f999f1f93247eb2fd7f7658ba13
Katie Barnett <katiebarnett5@gmail.com>
2024-04-14 21:34:25 +1000

    Demo - Add a column

--- app/src/main/java/dev/katiebarnett/myuncommoncompanion/MainActivity.kt
+++ app/src/main/java/dev/katiebarnett/myuncommoncompanion/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.katiebarnett.myuncommoncompanion.ui.theme.MyUncommonCompanionTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +24,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyUncommonCompanionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Content(
+                        name = stringResource(id = R.string.app_name),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +35,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Content(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ContentPreview() {
     MyUncommonCompanionTheme {
-        Greeting("Android")
+        Content(stringResource(id = R.string.app_name))
     }
 }
