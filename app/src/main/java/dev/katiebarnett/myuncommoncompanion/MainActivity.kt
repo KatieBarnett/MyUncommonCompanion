95e3fc543306d361b3fe49f656077183e2cd62a3
Katie Barnett <katiebarnett5@gmail.com>
2024-04-14 21:41:39 +1000

    Demo - Add a text field

--- app/src/main/java/dev/katiebarnett/myuncommoncompanion/MainActivity.kt
+++ app/src/main/java/dev/katiebarnett/myuncommoncompanion/MainActivity.kt
@@ -3,13 +3,19 @@ package dev.katiebarnett.myuncommoncompanion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,11 +41,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Content(name: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
+    var input by remember { mutableStateOf<String>("") }
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.padding(16.dp)
+    ) {
         Text(
             text = name,
             style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier
+        )
+        TextField(
+            value = input,
+            onValueChange = {
+                input = it
+            },
+            label = {
+                Text(text = "What type of pet would you like?")
+            }
         )
     }
 }
