1b525241704adc607c7ac691e175e4231215e8e0
Katie Barnett <katiebarnett5@gmail.com>
2024-04-16 21:18:55 +1000

    Demo - Add the Gradle Secrets plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
