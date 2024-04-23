8411be4576d10b1c924abb99446ac0cf999c88cf
Katie Barnett <katiebarnett5@gmail.com>
2024-04-23 21:28:25 +1000

    Demo - Parse the Json using Kotlin Serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.services)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -61,4 +62,7 @@ dependencies {
     // Firebase AI SDK
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.ai)
+
+    // Kotlin Serialization
+    implementation(libs.kotlinx.serialization.json)
 }
