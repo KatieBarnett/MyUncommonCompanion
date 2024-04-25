f88552c0a223647cfff9cadca8f2bdda384848b5
Katie Barnett <katiebarnett5@gmail.com>
2024-04-25 12:16:31 +1000

    Demo - Modify the model generationConfig & safetySettings to produce different results.

--- app/src/main/java/dev/katiebarnett/myuncommoncompanion/MainActivity.kt
+++ app/src/main/java/dev/katiebarnett/myuncommoncompanion/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.katiebarnett.myuncommoncompanion
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -28,7 +29,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.firebase.Firebase
 import com.google.firebase.ai.ai
+import com.google.firebase.ai.type.GenerationConfig
 import com.google.firebase.ai.type.GenerativeBackend
+import com.google.firebase.ai.type.HarmBlockThreshold
+import com.google.firebase.ai.type.HarmCategory
+import com.google.firebase.ai.type.SafetySetting
 import dev.katiebarnett.myuncommoncompanion.ui.theme.MyUncommonCompanionTheme
 import kotlinx.coroutines.launch
 
@@ -63,14 +68,30 @@ fun Content(
     var firstTextChangeHobbies by remember { mutableStateOf<Boolean>(firstTextChangeInitialValue) }
     var firstTextChangeFamily by remember { mutableStateOf<Boolean>(firstTextChangeInitialValue) }
 
-    val input by remember { derivedStateOf {
-        "Recommend one pet for me that is not a usual domesticated animal, I live in $home, I like to $hobbies and my family is $family."
+    val input by remember {
+        derivedStateOf {
+            "Recommend one pet for me that is not a usual domesticated animal, I live in $home, I like to $hobbies and my family is $family."
         }
     }
 
     val coroutineScope = rememberCoroutineScope()
+
+    val harassmentSafety = SafetySetting(HarmCategory.HARASSMENT, HarmBlockThreshold.ONLY_HIGH)
+    val hateSpeechSafety = SafetySetting(HarmCategory.HATE_SPEECH, HarmBlockThreshold.MEDIUM_AND_ABOVE)
+
+    val configBuilder = GenerationConfig.builder()
+    configBuilder.temperature = 0.9f
+    configBuilder.topK = 16
+    configBuilder.topP = 0.1f
+    // configBuilder.maxOutputTokens = 200
+    // configBuilder.stopSequences = listOf("dog", "cat")
+
     val generativeModel = Firebase.ai(backend = GenerativeBackend.googleAI())
-        .generativeModel("gemini-2.5-flash")
+        .generativeModel(
+            modelName = "gemini-2.5-flash",
+            generationConfig = configBuilder.build(),
+            safetySettings = listOf(harassmentSafety, hateSpeechSafety)
+        )
 
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -143,11 +164,15 @@ fun Content(
         Button(
             onClick = {
                 coroutineScope.launch {
-                    result = generativeModel.generateContent(input).text.orEmpty()
+                    try {
+                        result = generativeModel.generateContent(input).text.orEmpty()
+                    } catch (e: Exception) {
+                        Log.e("ERROR", "Error fetching result", e)
+                    }
                 }
             },
-            enabled = firstTextChangeHome && firstTextChangeHobbies && firstTextChangeFamily
-                    && !home.isNullOrBlank() && !hobbies.isNullOrBlank() && !family.isNullOrBlank()
+            enabled = firstTextChangeHome && firstTextChangeHobbies && firstTextChangeFamily &&
+                !home.isNullOrBlank() && !hobbies.isNullOrBlank() && !family.isNullOrBlank()
         ) {
             Text(text = "Submit")
         }
