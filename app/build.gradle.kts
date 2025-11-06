fefa8bb9295847650cf144a76207ae630842d532
Katie Barnett <katiebarnett5@gmail.com>
2025-11-06 20:48:33 +1100

    Demo - Add Google Services Plugin & Firebase AI Logic SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -56,4 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Firebase AI SDK
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.ai)
 }
